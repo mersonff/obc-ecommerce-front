36cfafcaf91e7cd569e67ce266c183fd16dd4db7
Emerson Freitas <mersonsi@gmail.com>
2022-01-18 15:40:07 -0300

    add login form

---
+++ components/shared/BlueBackground/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../../../styles/Background.module.css';
+
+
+const BlueBackground: React.FC = ({ children }) => {
+  return (
+    <div className={styles.main}>
+      {children}
+    </div>
+  )
+}
+
+export default BlueBackground;
